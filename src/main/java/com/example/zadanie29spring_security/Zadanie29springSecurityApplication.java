250ad7b3f6b43a05cfd55a3089a348a806dbd00d
Frodo1996 <frodson35@gmail.com>
2024-01-16 16:52:50 +0100

    początek zadania 29

---
+++ src/main/java/com/example/zadanie29spring_security/Zadanie29springSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.zadanie29spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie29springSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie29springSecurityApplication.class, args);
+    }
+
+}
